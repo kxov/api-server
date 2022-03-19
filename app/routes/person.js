240352c55e511e3868b1d2353c6f7326f785d2d1
makx <dev.maksim.t@alldigitalads.com>
2022-03-19 16:56:02 +0200

    add brackets loop

--- app/routes/person.js
+++ app/routes/person.js
@@ -6,7 +6,9 @@ const fs = require('fs');
 
 const receiveArgs = async (req) => {
   const buffers = [];
-  for await (const chunk of req) buffers.push(chunk);
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
   const data = Buffer.concat(buffers).toString();
   return JSON.parse(data);
 };
