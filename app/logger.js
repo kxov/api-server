d3e65033f0721e6cd7704ca463195ea006296dba
makx <dev.maksim.t@alldigitalads.com>
2022-05-18 21:39:41 +0300

    use template string

--- app/logger.js
+++ app/logger.js
@@ -2,7 +2,7 @@
 
 const logger = (req) => {
   const date = new Date().toISOString();
-  console.log([date, req.method, req.url].join('  '));
+  console.log(`${date} ${req.method} ${req.url}`);
 };
 
 module.exports = { logger };
